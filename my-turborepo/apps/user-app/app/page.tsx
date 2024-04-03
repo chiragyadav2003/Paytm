0304736cc02114982b43d116b4bf007ab8815571
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-04 00:53:20 +0530

    init user-app and add tailwind

---
+++ my-turborepo/apps/user-app/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center bg-black text-white">
+      <div className="text-3xl">Paytm user app</div>
+    </div>
+  );
+}
