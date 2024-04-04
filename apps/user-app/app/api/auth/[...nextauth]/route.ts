c87093ed82972be3efa932ea2d95631798a4c42b
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-05 01:45:21 +0530

    added next auth

---
+++ apps/user-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,7 @@
+import NextAuth from "next-auth"
+import { authOptions } from "../../../lib/auth" 
+
+const handler = NextAuth(authOptions)
+
+export const GET = handler;
+export const POST = handler;
---
+++ apps/user-app/app/lib/auth.ts
@@ -0,0 +1,69 @@
+import db from "@repo/db/client"
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+    providers:[
+
+        CredentialsProvider({
+
+            name:'Credentials',
+            
+            credentials:{
+                phone:{label:"Phone number", type:"text", placeholder:"1234312343"},
+                password:{label:"Password", type:"password", placeholder:"............"}
+            },
+             // TODO: User credentials type from next-aut
+            async authorize(credentials:any){
+                //TODO: zod and otp validation
+                const hashedPassword = await bcrypt.hash(credentials.password, 10);
+                const existingUser = await db.user.findFirst({
+                    where:{
+                        number:credentials.phone
+                    }
+                });
+
+                if(existingUser){
+                    const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password)
+
+                    if(passwordValidation){
+                        return {
+                            id:existingUser.id.toString(),
+                            name:existingUser.name,
+                            email:existingUser.number //BUG: check error  
+                        }
+                    }
+                    return null;
+                }
+
+                try {
+                    const user = await db.user.create({
+                        data:{
+                            number:credentials.phone,
+                            password:hashedPassword
+                        }
+                    });
+
+                    return {
+                        id:user.id.toString(),
+                        name:user.name,
+                        email:user.number
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+
+                return null;
+            }
+
+        })
+    ],
+    secret:process.env.JWT_SECRET || "secret",
+    callbacks:{
+        //TODO: fix type here
+        async session({token,session}:any){
+            session.user.id = token.sub
+            return session
+        }
+    }
+} 
