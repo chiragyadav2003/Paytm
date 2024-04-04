b772f9068b6d60dee238f8a1aae07f7d9c722f22
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-05 01:05:48 +0530

    updating schema

---
+++ packages/db/prisma/migrations/20240404193419_update/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[number]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `number` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "AuthType" AS ENUM ('Google', 'Github');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "number" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ALTER COLUMN "email" DROP NOT NULL,
+ALTER COLUMN "name" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Merchant" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "auth_type" "AuthType" NOT NULL,
+
+    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Merchant_email_key" ON "Merchant"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_number_key" ON "User"("number");
