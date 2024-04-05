d14c1187e81d62d5bf810d3f134a0c0e57f27bda
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-05 17:25:18 +0530

    adding abbar in ui

---
+++ packages/ui/src/Appbar.tsx
@@ -0,0 +1,22 @@
+import { Button } from "./button";
+
+interface AppbarProps{
+    user ?: {
+        name ?:string|null
+    },
+    onSignin:any,
+    onSignout:any
+}
+
+export const Appbar = ({user,onSignin,onSignout}:AppbarProps)=>{
+    return (
+        <div className="flex justify-between border-b px-4">
+        <div className="text-lg flex flex-col justify-center">
+            PayTM
+        </div>
+        <div className="flex flex-col justify-center pt-2">
+            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
+        </div>
+    </div>
+    )
+}
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,19 @@
-"use client";
+"use client"
 
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+interface ButtonProps{
+  children   : React.ReactNode;
+  onClick     : ()=>void;
+  classname  ?: string
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({onClick,children,classname="empty"}:ButtonProps)=>{
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={onClick}
+      type="button"
+      className={classname!=='empty'?classname:"text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"}
     >
       {children}
     </button>
-  );
-};
+  )
+}
