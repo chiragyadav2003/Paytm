d3b82d4a76acaf36de777233a1dc5a924eab2569
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-05 17:26:44 +0530

    configuring user app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+import { signIn,signOut,useSession } from "next-auth/react"
+import {Appbar} from "@repo/ui/appbar"
 
-export default function Page(): JSX.Element {
+export default function Page():JSX.Element{
+  const session = useSession();
   return (
-    <div className="flex flex-col h-screen items-center justify-center bg-black text-white">
-      <div className="text-3xl">Paytm user app</div>
+    <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user||"anonymous"}/>
     </div>
-  );
+  )
 }
+
+
+
+
+// export default function Page(): JSX.Element {
+//   return (
+//     <div className="flex flex-col h-screen items-center justify-center bg-black text-white">
+//       <div className="text-3xl">Paytm user app</div>
+//     </div>
+//   );
+// }
