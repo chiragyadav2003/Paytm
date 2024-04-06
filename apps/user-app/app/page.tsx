ddfbf392af24af3a0b17e873bc3cc52e45788ba2
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-06 22:38:06 +0530

    adding home page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,21 +2,20 @@
 import { signIn,signOut,useSession } from "next-auth/react"
 import {Appbar} from "@repo/ui/appbar"
 
-export default function Page():JSX.Element{
+export default function Page(): JSX.Element{
   const session = useSession();
   return (
     <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user||"anonymous"}/>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}/>
     </div>
   )
 }
 
 
-
-
 // export default function Page(): JSX.Element {
 //   return (
 //     <div className="flex flex-col h-screen items-center justify-center bg-black text-white">
+//       <Button onClick={()=>console.log("hoi")} classname="bg-red-400 text-lg rounded-xl px-6 py-2">Hello</Button>
 //       <div className="text-3xl">Paytm user app</div>
 //     </div>
 //   );
