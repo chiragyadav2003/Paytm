b6ccb73df5a3f7dfeb4241bfcf557d45ad2cafe5
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-09 15:37:06 +0530

    Add dependencies and update webhook logic in bank-webhook package

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,15 +1,61 @@
 import express from "express";
+import db from "@repo/db/client"
 
 const app = express()
+app.use(express.json())
+interface paymentInformationInterface{
+    token : string;
+    userId: string;
+    amount: string
+}
 
-app.post("/hdfcWebhook", (req,res)=>{
-    //TODO: add zod validation
+app.post("/hdfcWebhook", async(req,res)=>{
 
-    const paymentInformation = {
+    //TODO -  add zod validation
+    // TODO - check if this request is came from hdfc bank, add a webhook secret
+
+    const paymentInformation:paymentInformationInterface = {
         token:req.body.token,
         userId:req.body.user_identifier,
         amount:req.body.amount
     };
 
-    //TODO: updater balance in db, add txn
+    //init transaction
+    try {
+        await db.$transaction([
+
+            //update user balance
+            db.balance.update({
+                where:{
+                    userId:Number(paymentInformation.userId)
+                },
+                data:{
+                    amount:{
+                        increment:Number(paymentInformation.amount)
+                    }
+                }
+            }),
+
+            //onRamp txn
+            db.onRampTransaction.update({
+                where:{
+                    token:String(paymentInformation.token)
+                },
+                data:{
+                    status:"Success"
+                }
+            })
+        ]);
+        res.status(200).json({
+            message:"Captured"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(411).json({
+            message:"Error while processing webhook"
+        })
+    }
+
 })
+
+app.listen(3003)
