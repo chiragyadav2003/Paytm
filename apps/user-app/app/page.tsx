c288ef49b9203aba6a79efa435f5d05d34dfb128
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-09 16:11:19 +0530

    fixing appbar

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,16 +1,16 @@
-"use client"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./lib/auth"
+import { redirect } from "next/navigation"
 
-import { signIn,signOut,useSession } from "next-auth/react"
-import {Appbar} from "@repo/ui/appbar"
+export default async function Page(){
+  const session = await getServerSession(authOptions)
+  
+  if(session?.user){
+    redirect("/dashboard")
+  }else{
+    redirect("/api/auth/signin")
+  }
 
-export default function Page(): JSX.Element{
-  const session = useSession();
-
-  return (
-    <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}/>
-    </div>
-  )
 }
 
 
