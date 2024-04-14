ae29c85267a77157c3d83d778cd13fd13c395ba7
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-14 23:53:41 +0530

    better fix - wrapping transaction in try-catch so that error can be handled easily on client side

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -4,86 +4,109 @@ import { authOptions } from "../auth"
 import { getServerSession } from "next-auth"
 import prisma from "@repo/db/client"
 
-export async function p2pTransfer(to:string,amount:number){
+export async function p2pTransfer(to: string, amount: number) {
     //to - it is phone number of receiver/beneficiary
     const session = await getServerSession(authOptions)
 
     const from = session?.user?.id;
-    if(!from){
+    if (!from) {
         return {
-            message:"error while sending"
-        }
+            message: "Error while sending: User not authenticated",
+            success: false
+        };
     }
 
     const toUser = await prisma.user.findFirst({
-        where:{
-            number:String(to)
+        where: {
+            number: String(to)
         }
     });
 
-    if(!toUser){
+    if (!toUser) {
         console.log("beneficiary not exist")
-        throw new Error ("User not found")
+        return {
+            message: "Error while sending: Beneficiary not found",
+            success: false
+        };
     };
 
-    await prisma.$transaction(async(tx)=>{
+    try {
+        await prisma.$transaction(async (tx) => {
 
-        //NOTE - row locking
-        // tx.$queryRaw is a method provided by Prisma to execute raw SQL queries within a transaction context.
-        // The FOR UPDATE clause is used to apply an exclusive lock on the selected rows. This ensures that other transactions attempting to read or modify the same rows will be blocked until the lock is released.
+            //NOTE - row locking
+            // tx.$queryRaw is a method provided by Prisma to execute raw SQL queries within a transaction context.
+            // The FOR UPDATE clause is used to apply an exclusive lock on the selected rows. This ensures that other transactions attempting to read or modify the same rows will be blocked until the lock is released.
 
-        //NOTE: prisma does not support lock out of the box, so we will use this syntax
-        
-        await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId"=${Number(from)} FOR UPDATE`
+            //NOTE: prisma does not support lock out of the box, so we will use this syntax
 
-        //* this syntax give error
-        // await tx.balance.findUnique({
-        //     where:{
-        //         userId:Number(from)
-        //     },
-        //     lock:{
-        //         mode:'update'
-        //     }
-        // })
+            await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId"=${Number(from)} FOR UPDATE`
 
-        const fromBalance = await tx.balance.findUnique({
-            where:{
-                userId:Number(from)
+            //* this syntax give error
+            // await tx.balance.findUnique({
+            //     where:{
+            //         userId:Number(from)
+            //     },
+            //     lock:{
+            //         mode:'update'
+            //     }
+            // })
+
+            const fromBalance = await tx.balance.findUnique({
+                where: {
+                    userId: Number(from)
+                }
+            });
+
+            if (!fromBalance || fromBalance.amount < amount) {
+                throw new Error("insufficient balance")
             }
-        });
 
-        if(!fromBalance || fromBalance.amount<amount){
-            throw new Error("Insufficient balance")
+            // console.log("before sleep")
+            // await new Promise(resolve=>setTimeout(resolve,5000))
+            // console.log("after sleep")
+
+            await tx.balance.update({
+                where: {
+                    userId: Number(from)
+                },
+                data: {
+                    amount: {
+                        decrement: amount
+                    }
+                }
+            })
+            await tx.balance.update({
+                where: {
+                    userId: toUser.id
+                },
+                data: {
+                    amount: {
+                        increment: amount
+                    }
+                }
+            })
+            //entry in db
+            await tx.p2pTransfer.create({
+                data: {
+                    fromUserId: Number(from),
+                    toUserId: Number(toUser.id),
+                    amount,
+                    timeStamp: new Date()
+                }
+            })
+        })
+    } catch (error) {
+        console.log("transaction failed")
+        return {
+            error: "transaction failed",
+            message: "insufficient balance",
+            sucess: false
         }
+    }
 
-        // console.log("before sleep")
-        // await new Promise(resolve=>setTimeout(resolve,5000))
-        // console.log("after sleep")
-
-        await tx.balance.update({
-            where:{
-                userId:Number(from)
-            },
-            data:{
-                amount:{
-                    decrement:amount
-                }
-            }
-        })
-
-        await tx.balance.update({
-            where:{
-                userId:toUser.id
-            },
-            data:{
-                amount:{
-                    increment:amount
-                }
-            }
-        })
-    })
     console.log("completed")
     return {
-        message:"txn completed"
+        message: "txn completed",
+        success: true
     }
 }
