cae91c68c69ccc028a165b8e733073407e6bf543
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-18 19:03:13 +0530

    check new workflow

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -5,6 +5,9 @@ export default function dashboard() {
             {/* TODO - add transaction feature */}
             Hi, i am text for checking ci workflow
             I am text from sample workflow
+            <div>
+                Hello, i am here to check updated workflow
+            </div>
         </div>
     )
 }
